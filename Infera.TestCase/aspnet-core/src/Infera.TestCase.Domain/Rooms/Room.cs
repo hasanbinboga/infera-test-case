a20c0876b7549e477cb4b9969a93b9d41ac7f89a
Hasan BINBOGA <hasanbinboga@gmail.com>
2024-02-15 11:24:26 +0300

    Create Room.cs

---
+++ Infera.TestCase/aspnet-core/src/Infera.TestCase.Domain/Rooms/Room.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Volo.Abp.Domain.Entities.Auditing;
+
+namespace Infera.TestCase
+{
+    public class Room : FullAuditedAggregateRoot<Guid>
+    {
+        public virtual Guid BuildingId { get; set; }
+        public virtual int Number { get; set; }
+        public virtual int Capacity { get; set; }
+        public virtual int Floor { get; set; }
+        public virtual bool HasMiniBar { get; set; }
+        public virtual string? Notes { get; set; }
+
+        public Room()
+        {
+            
+        }
+    }
+}
